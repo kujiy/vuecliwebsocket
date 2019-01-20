3200e9eb5a7fd4af6df410d7a91b7832cedd507b
s092018 <s092018@gmail.com>
2019-01-20 14:33:40 +0900

    replace component to websocket

---
+++ frontend/src/components/WebSocket.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <div v-for="message in messages" :key=message :value=message>
+          {{message}}
+      </div>
+  </div>
+</template>
+
+<script>
+//@ts-check
+export default {
+  data() {
+    return {
+      messages: []
+    };
+  },
+  created() {
+      const ws = this.ws = new WebSocket(`ws://${location.host}/websocket`);
+      ws.onopen = () => {
+      }
+      ws.onmessage = (message) => {
+          this.messages.push(message);
+      }
+  }
+};
+</script>
+
+<style>
+</style>
